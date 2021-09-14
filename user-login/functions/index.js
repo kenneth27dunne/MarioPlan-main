bfceeb1de600efc0c8b6688c5848cb3354e4b41a
kenneth27dunne <kenneth_27_dunne@msn.com>
2021-09-14 12:33:32 +0100

    Completed Course
    Completed the net ninja react, redux & firebase course

--- user-login/functions/index.js
+++ user-login/functions/index.js
@@ -1,10 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello, Ninjas");
-});
+admin.initializeApp();
 
 const createNotification = notification => {
   return admin.firestore().collection('notifications')
@@ -12,14 +8,29 @@ const createNotification = notification => {
   .then(doc => console.log('notification added', doc))
 }
 
-exports.projectCreated = functions.firestore
-.document('project/{projectId}')
+exports.projectCreated = functions.region('europe-west2').firestore
+.document('projects/{projectId}')
 .onCreate(doc => {
   const project = doc.data();
   const notification = {
     content: 'added a new project',
     user: `${project.authorFirstName} ${project.authorLastName}`,
-    time: admin.firestore.FieldValue.serverTimeStamp()
+    time: admin.firestore.FieldValue.serverTimestamp()
   }
   return createNotification(notification)
 })
+
+exports.userJoined = functions.region('europe-west2').auth.user()
+  .onCreate(user => {
+      return admin.firestore().collection('users')
+      .doc(user.uid).get()
+      .then(doc => {
+        const newUser = doc.data();
+        const notification = {
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          content: 'Joined the party',
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification);
+      })
+  })
--- user-login/src/Components/dashboard/Notifications.js
+++ user-login/src/Components/dashboard/Notifications.js
@@ -1,15 +1,24 @@
 import React from "react";
+import moment from 'moment'
 
-const Notifications = () => {
+const Notifications = (props) => {
+    const {notifications} = props;
+
     return(
         <div className="section">
             <div className="card z-depth-0">
                 <div className="card-content">
                     <span className="card-title">Notifications</span>
                     <ul className="notifications">
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
+                        {notifications && notifications.map(item => {
+                            return (<li key={item.id}>
+                                <span className="pink-text">{item.user} </span>
+                                <span>{item.content}</span>
+                                <div className="grey-text note-date">
+                                    {moment(item.time.toDate()).fromNow()}
+                                </div>
+                            </li>)
+                        })}
                     </ul>
                 </div>
             </div>
